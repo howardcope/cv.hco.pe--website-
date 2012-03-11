1e772496ed065f9cd67b8e24195af1d213ef2281
hoovard <hoovard@hoovard-ThinkPad-L512.(none)>
2012-03-11 10:19:28 +0000

    cv web commit 11032012

---
+++ index (copy).php
@@ -0,0 +1,99 @@
+<?php
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <title>hco.pe | cv</title>
+        <style type="text/css">
+            
+             body
+             {
+                 font-family:courier;
+                 font-size: 1m;
+             }
+             
+             table#cv 
+             {
+                width:600px;                
+             }
+             
+             table#cv th
+             {
+                background-color:#999;  
+                border:1px solid #000; 
+                color:#eee;
+                text-align:left;
+             }
+             
+             table#cv td.section
+             {
+                background-color:#ccc;  
+                border:1px solid #666; 
+                color:#333;
+             }
+             
+             table#cv td.info
+             {
+                background-color:#eee;  
+                border:1px solid #999; 
+             }
+             
+            
+        </style>
+    </head>
+    <body>
+        <table id="cv">
+            <tr><th>cv.hco.pe</th></tr>
+            <tr><td class="section">details</td></tr>
+            <tr><td class="info">
+                Howard Cope<br>
+                100B Earlham Road<br>
+                Norwich<br>
+                NR2 3HB <br>
+                DOB: 22nd September 1963<br>
+                <br>
+                m: +44 (0) 7905 411 655<br>
+                e: <a href="mailto:howardc.cope@gmail.com">howardc.cope@gmail.com</a>
+            <tr><td class="section">DISCIPLINES</td></tr>
+            <tr><td class="info">
+                <ul>
+                <li>viz|rt realtime broadcast graphics systems</li>
+                <li>Interface Design and programming</li>
+                <li>Web Accessibility issues</li>
+                <li>CSS</li>
+                <li>Internet Design</li>
+                <li>Database Design and Programming</li>
+                <li>Print Design</li>
+                <li>Illustration</li>
+                <li>Multimedia CD ROM Graphic Design, Interface Design and Authoring</li>
+                <li>Digital Video Editing</li>
+                <li>2D + 3D Animation</li>
+                <li>Typography</li>
+                </ul>
+            </td></tr>
+            
+            <tr><td class="section">CHRONOLOGY</td></tr>
+            <tr><td class="info">
+            <ul>
+                <li>2011 - Present: Bloomberg TV - vizrt realtime data graphics & systems specialist</li>
+                <li>2007 - 2011: Spotted Zebra Ltd - Graphic designer/programmer/vizrt consultant</li>
+                <li>2001 - 2006: Accent Design Group - Internet designer/programmer, Illustrator</li>
+                <li>1998 - 2001: Anglia Campus - Internet Graphic Design, CD ROM Design & Authoring</li>
+                <li>1997 - 1998: Anglia Interactive - Internet Graphic Design</li>
+                <li>1995 - 1997: Anglia Multimedia - CD ROM Graphic Design</li>
+                <li>1994 - 1995: Arnatt Abrey Design (Corporate Design) - Print & Annual Report design</li>
+                <li>1991 - 1994: BA(Hons) Graphic Design 2.1 - Norfolk Institute of Art and Design</li>
+                <li>1989 - 1991: BTech National Diploma Graphic Design - N E Essex School of Art and Design</li>
+            </ul>
+            </td></tr>
+     
+            
+            <tr><td class="section">s</td></tr>
+            <tr><td class="info">
+            i
+            </td></tr>
+            </td></tr>
+        </table>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,149 @@
+<?php
+    
+    function entitize($str) 
+    {
+		$obfuscated = "";							
+		$len = strlen($str);		
+		for ($n = 0; $n < $len; ++$n) 
+		{
+			$obfuscated .= "&#".ord($str{$n}).";";
+		}
+		return $obfuscated;
+	}
+
+    $filename = $_SERVER['SCRIPT_FILENAME'];
+    $strLastUpdated =  date ("F d Y H:i:s", filemtime($filename));
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <title>hco.pe | cv</title>
+        <link rel=StyleSheet href="styles_screen.css" type="text/css" media=screen>
+        
+    </head>
+    <body>
+        <ul id="cv">
+            <li class="header"><h1>cv.hco.pe</h1></li>
+            <li class="section"><h2>DETAILS</h2></li>
+            <ul class="info">
+                <li>Howard Cope</li>
+                <li>100B Earlham Road</li>
+                <li>Norwich</li>
+                <li>NR2 3HB </li>
+                <li>DOB: 22nd September 1963</li>
+                
+                <li>m: +44 (0) 7905 411 655</li>
+                <li>e: <a href="<?php echo entitize('mailto:msg@hco.pe') ?> "><?php echo entitize("msg@hco.pe") ?></a></li>
+            </ul>
+            <li class="section"><h2>CHRONOLOGY</h2></li>
+            <ul class="info">
+                <li><strong>2011 - Present:</strong> Bloomberg TV - vizrt realtime data graphics & systems specialist</li>
+                <li><strong>2007 - 2011:</strong> Spotted Zebra Ltd - Graphic designer/programmer/vizrt consultant/webmaster</li>
+                <li><strong>2001 - 2006:</strong> Accent Design Group - Internet designer/programmer, Illustrator</li>
+                <li><strong>1998 - 2001:</strong> Anglia Campus - Internet Graphic Design, CD ROM Design & Authoring</li>
+                <li><strong>1997 - 1998:</strong> Anglia Interactive - Internet Graphic Design</li>
+                <li><strong>1995 - 1997:</strong> Anglia Multimedia - CD ROM Graphic Design</li>
+                <li><strong>1994 - 1995:</strong> Arnatt Abrey Design (Corporate Design) - Print & Annual Report design</li>
+                <li><strong>1991 - 1994:</strong> BA(Hons) Graphic Design 2.1 - Norfolk Institute of Art and Design</li>
+                <li><strong>1989 - 1991:</strong> BTech National Diploma Graphic Design - N E Essex School of Art and Design</li>
+            </ul>
+            <li class="section"><h2>DISCIPLINES</h2></li>
+            <ul class="info">
+                <li>viz|rt realtime broadcast graphics systems</li>
+                <li>Interface Design and programming</li>
+                <li>Web Accessibility issues</li>
+                <li>CSS</li>
+                <li>Internet Design</li>
+                <li>Database Design and Programming</li>
+                <li>Print Design</li>
+                <li>Illustration</li>
+                <li>Multimedia CD ROM Graphic Design, Interface Design and Authoring</li>
+                <li>Digital Video Editing</li>
+                <li>2D + 3D Animation</li>
+                <li>Typography</li>
+            </ul>
+            
+            <li class="section"><h2>SOFTWARE</h2></li>
+            <ul class="info">
+                <li>Info Item</li>               
+            </ul>
+            
+            <li class="section"><h2>SKILLS</h2></li>
+            <ul class="info">
+                <li>Info Item</li>               
+            </ul>
+            
+            <li class="section"><h2>PROJECTS</h2></li>
+            <ul class="info">
+                <li class="subsection"><h3>Bloomberg TV<h3></li>  
+                <li>Info Item</li>  
+                <li class="subsection"><h3>Spotted Zebra Ltd<h3></li>  
+                <li>www.spotted-zebra.com website - CSS, Javascript, database design, CMS design and programming including multi user timesheet system (PHP, MySQL)</li>  
+                <li>Blackberry - Control interface for Ventuz presentation using C# & MS Access DB</li>  
+                <li>Siemens - Control interface for Ventuz using C# & MS Access DB</li>  
+                <li>TV2 Odense, Denmark - Creation of touchscreen solutions and realtime election graphics, onsite in Odense
+                <p>http://www.youtube.com/watch?v=kgKP9VaWBl0</p>
+                <p>http://www.youtube.com/watch?v=xnwKI7GAEB4</p>
+                </li>  
+                <li>CNBC Africa - Financial realtime data graphics onsite in Johannesburg</li>  
+                <li>VIASAT4, Norway - conversion of concept artwork in to viz graphics/transition logic scenes for newly starting Norwegian satellite news channel - 2 weeks start to finish onsite in Oslo</li>  
+                <li>Info Item</li>  
+                <li>Info Item</li>  
+                <li>Info Item</li>  
+                <li class="subsection"><h3>Info Subsection<h3></li>  
+                <li>Info Item</li>  
+                <li>Info Item</li>  
+                <li>Info Item</li>               
+            </ul>
+            
+            <li class="section"><h2>INTERESTS</h2></li>
+            <ul class="info">
+                <li>Info Item</li>               
+            </ul>
+            
+            <li class="section"><h2>SECTION TITLE</h2></li>
+            <ul class="info">
+                <li>Info Item</li>               
+            </ul>
+            
+            <li class="section"><h2>SECTION TITLE</h2></li>
+            <ul class="info">
+                <li>Info Item</li>               
+            </ul>
+            
+        </ul>
+            <!--
+            <tr><td class="section">DISCIPLINES</td></tr>
+            <tr><td class="info">
+                <ul>
+                <li>viz|rt realtime broadcast graphics systems</li>
+                <li>Interface Design and programming</li>
+                <li>Web Accessibility issues</li>
+                <li>CSS</li>
+                <li>Internet Design</li>
+                <li>Database Design and Programming</li>
+                <li>Print Design</li>
+                <li>Illustration</li>
+                <li>Multimedia CD ROM Graphic Design, Interface Design and Authoring</li>
+                <li>Digital Video Editing</li>
+                <li>2D + 3D Animation</li>
+                <li>Typography</li>
+                </ul>
+            </td></tr>
+            
+            <tr><td class="section">CHRONOLOGY</td></tr>
+            <tr><td class="info">
+            <ul>
+                <
+            </ul>
+            </td></tr>
+     
+            
+            <tr><td class="section">s</td></tr>
+            <tr><td class="info">
+            i
+            </td></tr>
+            </td></tr>
+        </table>-->
+    </body>
+</html>
